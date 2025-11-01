d4bbb3837323533a47487365d2d4cf8874f3e6f8
Priyanshu Soyal <mr.priyanshu4678@gmail.com>
2025-11-01 23:53:15 +0530

    Add Projects

---
+++ 17-AIR-BNB/cloudConfig.js
@@ -0,0 +1,37 @@
+// const cloudinary = require("cloudinary").v2;
+// const { cloudinaryStorage } = require("multer-storage-cloudinary");
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.CLOUD_API_KEY,
+//   api_secret: process.env.CLOUD_API_SECRET,
+// });
+// const storage = new cloudinaryStorage {
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'wonderlust_DEV',
+//     allowedFormat: ['png', 'jpg', 'jpeg'],
+//   },
+// };
+
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "wonderlust_DEV",
+    allowed_formats: ["png", "jpg", "jpeg"],
+  },
+});
+
+module.exports = {
+  cloudinary,
+  storage,
+};
